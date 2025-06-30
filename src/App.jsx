903eda42eac484bb957d5e928030c66ce40f3be4
Agbaje Isaac Adekunle <adekunleisaac1989@gmail.com>
2025-06-30 17:14:04 +0100

    CD test: updated heading

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
        <SkillsSection />
        <Certifications />
       <ProjectsSection />
-      <ContactSection />
+      <Agbajeisaac />
       <Footer />
     </div>
   );
